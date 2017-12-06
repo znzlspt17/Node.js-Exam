3125f36bc5a4ac59a5027aeec1fc6ab8860def97
kimjinwoo <znzlspt17@gmail.com>
2017-12-06 17:45:54 +0900

    added expressExam

---
+++ NodeExample1/ch05_test8.js
@@ -0,0 +1,31 @@
+var http = require('http');
+
+var opts = {
+    host: 'www.google.com',
+    port: 80,
+    method: 'POST',
+    path: '/',
+    headers: {}
+};
+
+var resData = '';
+var req = http.request(opts, function (res) {
+    res.on('data', function (chunk) {
+        resData += chunk;
+    });
+
+    res.on('end', function () {
+        console.log('확인' + resData);
+    });
+});
+
+opts.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+req.data = "q=actor";
+opts.headers['Content-length'] = req.data.lenght;
+
+req.on('error', function (err) {
+    console.log('오류 발생' + err.message);
+});
+
+req.write(req.data);
+req.end();
